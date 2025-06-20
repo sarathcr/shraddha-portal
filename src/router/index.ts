918fdb1314b8129ccf61b50b244366da427e3487
scheerakkadan <scheerakkadan@opentrends.net>
2025-06-20 11:09:25 +0530

    implemented hash routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
 import DashboardView from '@/views/admin/dashboard/DashboardView.vue'
 import LoginView from '@/views/auth/login/LoginView.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/:pathMatch(.*)*', redirect: '/login' },
     {
